195a21ea3f03bda31183cddd4410c18c8929e347
IBakuda <zoxspirit@gmail.com>
2025-05-13 23:22:06 +0300

    update 11.3 upd app Serch_resourses

--- arknights/serchresourses/serchdata.py
+++ arknights/serchresourses/serchdata.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-def collect_data(resorse, en):
+def collect_data(resorse, en, w):
     output = {}
 
     for i, td in enumerate(resorse, start=en):
-        for div in td.find_all('div'):
+        for div in td.find_all('div', recursive=w):
 
             item = div.find('div', class_='item-tooltip')
             quantity_div = div.find('div', class_='quantity')
@@ -33,16 +33,16 @@ def serchoperator(name):
     modeules = soup.find_all('table', class_='mrfz-wtable operator-module-prereq')
 
     promotion_tds = promotion.find_all('td')
-    promotion_resources = collect_data(promotion_tds, 1)
+    promotion_resources = collect_data(promotion_tds, 1, True)
 
     skills_tds = skils.find_all('td')
-    skils_resourses = collect_data(skills_tds, 2)
+    skils_resourses = collect_data(skills_tds, 2, True)
 
 
     modeules_resourses = {}
     for enum, item in enumerate(modeules, start=1):
         module_tds = item.find_all('td')
-        modeules_resourses[enum] = collect_data(module_tds, 1)
+        modeules_resourses[enum] = collect_data(module_tds, 1, False)
 
     data ={
         'promotion_resourses': promotion_resources,
