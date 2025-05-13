42491e46d626be6026ada86ec2cca84925b9da31
IBakuda <zoxspirit@gmail.com>
2025-05-13 22:42:57 +0300

    update 11.2 upd app Serch_resourses

--- arknights/serchresourses/serchdata.py
+++ arknights/serchresourses/serchdata.py
@@ -1,6 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
+def collect_data(resorse, en):
+    output = {}
+
+    for i, td in enumerate(resorse, start=en):
+        for div in td.find_all('div'):
+
+            item = div.find('div', class_='item-tooltip')
+            quantity_div = div.find('div', class_='quantity')
+
+            if quantity_div and item:
+
+                item_found = item.get('data-name', 'Неизвестно')
+                quantity_found = quantity_div.text.strip()
+
+                if i not in output:
+                    output[i] = {}
+
+                    # Добавляем item: quantity
+                output[i][item_found] = quantity_found
+    return output
+
 
 def serchoperator(name):
     url = f'https://arknights.wiki.gg/wiki/{name}'
@@ -8,24 +29,25 @@ def serchoperator(name):
     soup = BeautifulSoup(responce.content, 'lxml')
 
     promotion = soup.find('table', id='operator-promotion-table')
-    skils = soup.find('div', class_='mw-collapsible-content') # Попробуем найти <td> если не получится <tr>
-    modeules = soup.find('table', class_='mrfz-wtable operator-module-prereq')
+    skils = soup.find('div', class_='mw-collapsible-content')
+    modeules = soup.find_all('table', class_='mrfz-wtable operator-module-prereq')
 
-    # promotion_resourses = promotion.find_all('td')
+    promotion_tds = promotion.find_all('td')
+    promotion_resources = collect_data(promotion_tds, 1)
 
-    promotion_resources = []
-    for td in promotion.find_all('td'):
-        quantity_div = td.find('div', class_='quantity')
-        item_img = td.find('img')
-        if quantity_div or item_img:
-            promotion_resources.append({
-                'item': item_img.get('alt', 'Неизвестно'),
-                'quantity': quantity_div.text.strip()
-            })
+    skills_tds = skils.find_all('td')
+    skils_resourses = collect_data(skills_tds, 2)
 
+
+    modeules_resourses = {}
+    for enum, item in enumerate(modeules, start=1):
+        module_tds = item.find_all('td')
+        modeules_resourses[enum] = collect_data(module_tds, 1)
 
     data ={
-        'promotion_resourses': 1
+        'promotion_resourses': promotion_resources,
+        'skils_resourses': skils_resourses,
+        'modeules_resourses': modeules_resourses,
     }
 
-    return [promotion_resources]
+    return data
